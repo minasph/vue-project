8a4e4dd1970e83141446a1d190f301e5d9071a55
minasph <tolstovamina@gmail.com>
2022-05-13 12:25:12 +0500

    first

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<script>
+import PostForm from "./components/PostForm.vue";
+import PostList from "./components/PostList.vue";
+
+export default {
+  components: {
+    PostList, PostForm
+  },
+  data() {
+    return {
+      posts: [
+        {id: 1, title: 'Javascript', body: 'Описание поста'},
+        {id: 2, title: 'Javascript 2', body: 'Описание поста 2'},
+        {id: 3, title: 'Javascript 3', body: 'Описание поста 3'},
+        {id: 4, title: 'Javascript 4', body: 'Описание поста 4'},
+      ],
+      dialogVisible: false,
+      modificatorValue: ''
+    }
+  },
+  methods: {
+    createPost(post) {
+      this.posts.push(post);
+      this.dialogVisible = false;
+    },
+    removePost(post) {
+      this.posts = this.posts.filter(p => p.id !== post.id);
+    },
+    showDialog() {
+      this.dialogVisible = true;
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="app">
+    <h1>Страница с постами</h1>
+    <my-button @click="showDialog" style="margin: 15px 0;">
+      Создать пост
+    </my-button>
+    <my-dialog
+        v-model:show="dialogVisible">
+      <post-form
+          @create="createPost"
+      />
+    </my-dialog>
+    <post-list
+        v-bind:posts="posts"
+        @remove="removePost"
+    />
+  </div>
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.app {
+  padding: 20px;
+  width: 1000px;
+  margin: 0 auto;
+}
+
+</style>
---
+++ src/components/PostItem.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="post">
+
+    <div>
+      <div><b>Название:</b> {{ post.title }}</div>
+      <div><b>Описание:</b> {{ post.body }}</div>
+    </div>
+
+    <div class="post__btns">
+      <my-button
+          @click="$emit('remove', post)"
+      >
+        Удалить
+      </my-button>
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PostItem",
+  props: {
+    post: {
+      type: Object,
+      required: true,
+    }
+  }
+}
+</script>
+
+<style scoped>
+.post {
+  padding: 15px;
+  border: 2px solid teal;
+  margin-top: 15px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
